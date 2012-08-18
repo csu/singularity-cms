ac37dc9ffbe32db58c72a3813c275c4508c4a68d
Christopher Su <christophersu9@gmail.com>
2012-08-19 01:33:08 +0800

    Added ability to specify strapdown.js location

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Singularity
 
-Singularity is a simple, flat file CMS which interprets marks up HTML using Markdown.
+Singularity is a simple, flat file CMS which marks up HTML using Markdown.
 
 ## URLs
 
 A file at content/index.html can be accessed at /. A file at content/text.txt can be accessed at /text. A file at content/sub/index.txt can be accessed at index.txt. A file at content/sub/text.txt can be accessed at /sub/text. If a file does not exist or cannot be found, content/404.txt will be used in its place. The content directory and other aspects of how Singularity handles URLs can be easily edited.
+
+## Markdown
+
+Singularity uses [strapdown.js](http://strapdownjs.com/) to mark up HTML. Strapdown.js also works with various Bootstrap themes.
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-
 <?php
 define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
 define('CONTENT_DIR', ROOT_DIR .'content/');
+
+//Change this to your strapdown.js location before using!
+//$strapdown_location = "/strapdown.js";
+$strapdown_location = "http://strapdownjs.com/v/0.1/strapdown.js";
+
 
 // Get request url and script url
 $url = '';
@@ -19,17 +23,15 @@ else $file = CONTENT_DIR .'index';
 if(is_dir($file)) $file = CONTENT_DIR . $url .'/index.txt';
 else $file .= '.txt';
 
+//Show 404 if file cannot be found
 if(file_exists($file)) $content = file_get_contents($file);
 else $content = file_get_contents(CONTENT_DIR .'404.txt');
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <title><?php echo ucwords(strtolower($url)); ?></title>
 <xmp theme="united" style="display:none;">
 <?php echo $content; ?>
 </xmp>
-<script src="http://strapdownjs.com/v/0.1/strapdown.js"></script>
-<!--<script src="/singularity-cms/strapdown.js"></script>-->
+<script src="<?php echo $strapdown_location; ?>"></script>
 </html>
